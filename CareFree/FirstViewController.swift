ab5e2e3945211be372cd56e65424450f392156f3
jevalen4868 <jeremyvalenzuela@j01.local>
2017-11-11 16:07:35 -0600

    Initial Commit

---
+++ CareFree/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CareFree
+//
+//  Created by Jeremy Valenzuela on 11/11/17.
+//  Copyright © 2017 Jeremy Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
