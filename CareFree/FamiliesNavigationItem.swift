24242bdc9390d28c76470d8a891ed833267b16e4
jevalen4868 <jevalen4868@gmail.com>
2017-11-19 21:08:19 -0600

    Cleanup

--- CareFree/FamiliesNavigationItem.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FamiliesNavigationItem.swift
-//  CareFree
-//
-//  Created by Jeremy Valenzuela on 11/18/17.
-//  Copyright © 2017 Jeremy Valenzuela. All rights reserved.
-//
-
-import Foundation
